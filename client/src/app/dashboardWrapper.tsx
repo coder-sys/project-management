ca459adda1f51df53e9f7a41955dec53b7eb1f1f
coder-sys <unix99493@gmail.com>
2025-05-25 16:21:28 -0500

    reverted

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -23,9 +23,14 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
-     
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+          isSidebarCollapsed ? "" : "md:pl-64"
+        }`}
+      >
         <Navbar />
         {children}
+      </main>
     </div>
   );
 };
@@ -34,6 +39,7 @@ const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
         <DashboardLayout>{children}</DashboardLayout>
+      
     </StoreProvider>
   );
 };
