b74fdf333384db08457099b249d61f1f1f34a008
coder-sys <unix99493@gmail.com>
2025-05-25 17:06:45 -0500

    mlc

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import AuthProvider from "./authProvider";
 import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
@@ -22,16 +23,12 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
-            <Sidebar />
-
       <main
         className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
           isSidebarCollapsed ? "" : "md:pl-64"
         }`}
       >
         <Navbar />
-                <Navbar />
-
         {children}
       </main>
     </div>
@@ -40,7 +37,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
+    <StoreProvider>
         <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
